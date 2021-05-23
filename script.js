2575842cffc86cd22ede44d7f6ad34971469f9b4
dev-davidgomes <davidsmithcrlh@outlook.com>
2021-05-23 01:06:33 -0300

    subindo desafio if e else

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let n1 = 18
+
+let souhumano = true
+
+
+
+
+
+if(n1 >=  18 && souhumano  ){
+    console.log("maior de 18")
+}else{
+    console.log("menor de 18")
+}
+
+
+
+let j  = true
+
+let d = true
+
+if (j == d &&  true ) {
+    console.log("faz aniversario em janeiro ou dezembro")
+
+}else {
+    console.log("nao faz aniversario em janeiro ou dezembro")
+};
+
+
+
+let n = "david"
+
+if (n.substring (0,1 === "d")){
+    console.log('começa com "d"')
+}else{
+    console.log('nao começa com "d"')
+}
+
+
+
